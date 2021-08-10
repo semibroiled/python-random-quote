ad0185c63cb15a2cf05b4bb7ac992dc315702ee3
semibroiled <amitavcmostafa@gmx.de>
2021-08-10 22:32:09 +0200

    Renamed main to primary

--- get-quote.py
+++ get-quote.py
@@ -1,6 +1,6 @@
-def main():
+def primary():
     print("Keep it logically awesome.")
-    
+
 
   #f = open("quotes.txt")
   #quotes = f.readlines()
@@ -9,4 +9,4 @@ def main():
   #print(quotes)
 
 if __name__== "__main__":
-  main()
+  primary()
